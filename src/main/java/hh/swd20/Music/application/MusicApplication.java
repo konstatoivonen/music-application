42a1afaacaf3403175d0c083ab8209b806b24d8e
Konsta Toivonen <konsta.toivonen@protonmail.com>
2020-04-21 18:23:54 +0300

    Added initial project files

---
+++ src/main/java/hh/swd20/Music/application/MusicApplication.java
@@ -0,0 +1,13 @@
+package hh.swd20.Music.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicApplication.class, args);
+	}
+
+}
